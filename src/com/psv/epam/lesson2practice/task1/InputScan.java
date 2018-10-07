e5a5e2ce12d34b469b542d88078dcd0ffe8e91c4
Serhii Prokopenko <serenkiy82@ukr.net>
2018-10-07 17:06:02 +0300

    reference to static variable through class name and not object

--- src/com/psv/epam/lesson2practice/task1/InputScan.java
+++ src/com/psv/epam/lesson2practice/task1/InputScan.java
@@ -6,9 +6,9 @@ public class InputScan {
     private static Scanner scan = new Scanner(System.in);
 
     public static long inputIntWithScanner(View view) {
-        view.printMessage(view.INPUT_INT_NUMBER);
+        view.printMessage(View.INPUT_INT_NUMBER);
         while (!scan.hasNextLong()) {
-            view.printMessage(view.WRONG_INPUT + view.INPUT_INT_NUMBER);
+            view.printMessage(View.WRONG_INPUT + View.INPUT_INT_NUMBER);
             scan.next();
         }
         return scan.nextLong();
